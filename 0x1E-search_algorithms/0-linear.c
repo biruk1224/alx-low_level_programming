a6476c3d8c5bc2bbd133a7047291be80a004d2de
biruk1224 <58214154+biruk1224@users.noreply.github.com>
2022-03-30 06:46:53 -0400

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,9 @@
 #include "search_algos.h"
 int linear_search(int *array, size_t size, int value){
       size_t i;
+	
+	if(array == NULL){return (-1);}
+	
 for (i = 0; (i < size) && (array); i++)
 	{	if (*(array + i) == value)
 		{
